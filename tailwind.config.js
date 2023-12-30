1a8f339629c1662bb302965b20ff58e3de215970
Bùi Quốc Huy <65577247+quochuy2405@users.noreply.github.com>
2023-12-30 20:44:24 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ["./src/**/*.{html,js,jsx,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				main: "#00B897",
+			},
+		},
+	},
+	plugins: [],
+};
