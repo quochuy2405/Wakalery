1a4bd5c79a0858c4e0931130dab0ec4d8572c20a
tech.huy.bui <tech.huy.bui@buymed.local>
2023-12-31 14:54:51 +0700

    update

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,9 +1,11 @@
+import { TextField } from "@/components/atoms";
 import { SideBar } from "@/components/organims";
 import { LoadingOutlined, PlusOutlined } from "@ant-design/icons";
-import { Segmented, Upload, message } from "antd";
+import { Button, Segmented, Select, Upload, message } from "antd";
 import type { UploadChangeParam } from "antd/es/upload";
 import type { RcFile, UploadFile, UploadProps } from "antd/es/upload/interface";
 import { useState } from "react";
+import { Form } from "react-router-dom";
 
 const getBase64 = (img: RcFile, callback: (url: string) => void) => {
 	const reader = new FileReader();
@@ -24,6 +26,7 @@ const beforeUpload = (file: RcFile) => {
 };
 const Profile = () => {
 	const [loading, setLoading] = useState(false);
+	const [tab, setTab] = useState<string>("Infomations");
 	const [imageUrl, setImageUrl] = useState<string>();
 
 	const uploadButton = (
@@ -93,64 +96,57 @@ const Profile = () => {
 							</div>
 						</div>
 						<div className='flex gap-2 px-2'>
-							<Segmented options={['Infomations', 'Contacts', "longtext-longtext-longtext-longtext"]} className="m-auto" block />
+							<Segmented
+								options={["Infomations", "Contacts"]}
+								onChange={(value) => {
+									setTab(value as string);
+								}}
+								className='w-[400px] m-auto'
+								block
+							/>
 						</div>
 					</div>
-					<div className='px-4 py-4'>
-						<div className='flex gap-2 items-center text-gray-800 dark:text-gray-300 mb-4'>
-							<svg
-								className='h-6 w-6 text-gray-600 dark:text-gray-400'
-								fill='currentColor'
-								xmlns='http://www.w3.org/2000/svg'
-								viewBox='0 0 24 24'
-								width='24'
-								height='24'>
-								<path
-									className=''
-									d='M12 12a5 5 0 1 1 0-10 5 5 0 0 1 0 10zm0-2a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm9 11a1 1 0 0 1-2 0v-2a3 3 0 0 0-3-3H8a3 3 0 0 0-3 3v2a1 1 0 0 1-2 0v-2a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v2z'
+					<div className='px-4 py-4 w-2/3 m-auto'>
+						{tab === "Infomations" && (
+							<Form className='flex flex-col gap-4 justify-center items-center w-full'>
+								<div className='flex justify-center gap-4 w-full'>
+									<TextField title='First name' />
+									<TextField title='Last name' />
+								</div>
+								<TextField title='Level' />
+								<Select
+									labelInValue
+									defaultValue={{ value: "lucy", label: "Lucy (101)" }}
+                 className="w-full h-12"
+									onChange={handleChange}
+									options={[
+										{
+											value: "jack",
+											label: "Jack (100)",
+										},
+										{
+											value: "lucy",
+											label: "Lucy (101)",
+										},
+									]}
 								/>
-							</svg>
-							<span>
-								<strong className='text-black dark:text-white'>12</strong> Followers you know
-							</span>
-						</div>
-						<div className='flex'>
-							<div className='flex justify-end mr-2'>
-								<img
-									className='border-2 border-white dark:border-gray-800 rounded-full h-10 w-10 -mr-2'
-									src='https://randomuser.me/api/portraits/men/32.jpg'
-									alt=''
-								/>
-								<img
-									className='border-2 border-white dark:border-gray-800 rounded-full h-10 w-10 -mr-2'
-									src='https://randomuser.me/api/portraits/women/31.jpg'
-									alt=''
-								/>
-								<img
-									className='border-2 border-white dark:border-gray-800 rounded-full h-10 w-10 -mr-2'
-									src='https://randomuser.me/api/portraits/men/33.jpg'
-									alt=''
-								/>
-								<img
-									className='border-2 border-white dark:border-gray-800 rounded-full h-10 w-10 -mr-2'
-									src='https://randomuser.me/api/portraits/women/32.jpg'
-									alt=''
-								/>
-								<img
-									className='border-2 border-white dark:border-gray-800 rounded-full h-10 w-10 -mr-2'
-									src='https://randomuser.me/api/portraits/men/44.jpg'
-									alt=''
-								/>
-								<img
-									className='border-2 border-white dark:border-gray-800 rounded-full h-10 w-10 -mr-2'
-									src='https://randomuser.me/api/portraits/women/42.jpg'
-									alt=''
-								/>
-								<span className='flex items-center justify-center bg-white dark:bg-gray-800 text-sm text-gray-800 dark:text-white font-semibold border-2 border-gray-200 dark:border-gray-700 rounded-full h-10 w-10'>
-									+999
-								</span>
-							</div>
-						</div>
+
+								<Button type='primary' className='bg-main button-form mt-6'>
+									Save
+								</Button>
+							</Form>
+						)}
+						{tab === "Contacts" && (
+							<Form className='flex flex-col gap-4 justify-center items-center w-full'>
+								<TextField title='Phone number' />
+								<TextField title='Address' />
+								<TextField title='URL Info' />
+
+								<Button type='primary' className='bg-main button-form mt-6'>
+									Save
+								</Button>
+							</Form>
+						)}
 					</div>
 				</div>
 			</div>
